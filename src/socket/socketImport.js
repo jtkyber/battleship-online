4d1367c31d53b2d78f507a67391f44d8f0cfb638
jtkyber <43868043+jtkyber@users.noreply.github.com>
2021-05-09 19:55:43 -0600

    fix

--- src/socket/socketImport.js
+++ src/socket/socketImport.js
@@ -1,2 +1,2 @@
 import io from 'socket.io-client';
-export const socket = io('https://safe-mesa-81492.herokuapp.com');       //,{autoConnect: false}
+export const socket = io('https://safe-mesa-81492.herokuapp.com/');       //,{autoConnect: false}
