39501d17c5201c4a2e74728bff3c950976433b02
jtkyber <43868043+jtkyber@users.noreply.github.com>
2022-02-09 22:50:50 -0500

    fix

--- src/components/boards/OpponentBoard.js
+++ src/components/boards/OpponentBoard.js
@@ -59,12 +59,8 @@ const OpponentBoard = ({ socket }) => {
         if (yourTurn && !e.target.classList.contains('hitMarker') && !e.target.classList.contains('missMarker')) {
             // setSquareClicked(e.target);
             setYourTurn(false);
-            const missileLaunchDuration = audio.missileLaunch.duration() * 1000 - 900;
             audio.missileLaunch.play();
-
-            setTimeout(() => {
-                socket.emit('send shot to opponent', {target: e.target.id, socketid: friendSocket});
-            }, missileLaunchDuration)
+            socket.emit('send shot to opponent', {target: e.target.id, socketid: friendSocket});
         }
     }
 
