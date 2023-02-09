aa96e8e32906039a25a63d0d40978480181312f0
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-02-09 16:41:12 -0500

    changed backend urls

--- src/socketImport.js
+++ src/socketImport.js
@@ -1,2 +1,2 @@
 import io from 'socket.io-client';
-export const socket = io(`${process.env.REACT_APP_API_URL}`);       //,{autoConnect: false}
+export const socket = io(`https://battleship-online-socket.vercel.app/`);       //,{autoConnect: false}
