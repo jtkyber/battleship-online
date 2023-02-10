d6a0c1521bbdfdd69b9a34fa54d7dabdccaf0e05
jtkyber <43868043+jtkyber@users.noreply.github.com>
2023-02-10 17:47:30 -0500

    fix

--- src/socketImport.js
+++ src/socketImport.js
@@ -1,2 +1,3 @@
 import io from 'socket.io-client';
-export const socket = io(`https://battleship-socket.adaptable.app/`);       //,{autoConnect: false} 
+export const socket = io(`http://localhost:3001`);       //,{autoConnect: false} 
+// http://localhost:3001
